8c37d5062c0534f8d90415c5d60cb93571a4d9f5
Prashant Agrawal <prashantagrawal2107@gmail.com>
2024-03-03 15:51:53 +0530

    React Components - II

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import MovieCard from "./Moviecard";
+import MovieList from "./MovieList";
+import Student from "./Student";
 
 function App() {
   return (
     <>
     <h1>MovieFlix</h1>
-    <MovieCard/>
+    {/* <MovieCard/> */}
+    <MovieList/>
+
+    {/* This is just for props demo
+    <Student name="Prashant" marks="80"/>
+    <Student name="Ravi" marks="90"/>
+    <Student name="Prateek" marks="95"/>
+    <Student/> */}
+
     </>
     );
+}
+
+Student.defaultProps = {
+  name : "Student",
+  marks : "N.A."
 }
 
 export default App;
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,70 +1,73 @@
 import React from "react";
 
 export default class MovieCard extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            title : " Sita Ramam",
-            plot:"A true Love Story.",
-            price : "Rs. 299",
-            rating : 9.5,
-            stars : 0
-        }
-        // Binding this to addStars to prevent loosing "this"....otherwise , this will become undefind.
-        // this.addStars = this.addStars.bind(this); // No need of binding...we are using arrow function now.
-    }
 
-    // addStars(){
+    // // addStars(){
+    // //     console.log(this.state);
+    // // }
+
+    // //  Arrow function automatically binds 'this'....no need explicit binding.
+    // addStars = ()=>{
+
+    //     if(this.state.stars>=5){
+    //         return ;
+    //     }
+    //     // setState() update the value as well as rerender the Component automatically....
+
+    //     // setState() can be used in two forms-->>
+
+    //     // FORM 1-->>
+    //     // this.setState({
+    //     //     stars : this.state.stars+=0.5
+    //     // });
+
+    //     // FORM 2-->>
+    //     this.setState((prevState)=>{
+    //         return {
+    //             stars : prevState.stars+=0.5
+    //         }
+    //     })
+
+    //     // this.state.stars+=0.5; // Can't simply do like this, we also have to rerender the Component
     //     console.log(this.state);
     // }
 
-    //  Arrow function automatically binds 'this'....no need explicit binding.
-    addStars = ()=>{
+    // minusStars = ()=>{
+    //     if(this.state.stars<=0){
+    //         return ;
+    //     }
+    //     this.setState((prevState)=>{
+    //         return {
+    //             stars : prevState.stars-=0.5
+    //         }
+    //     })
+    //     console.log(this.state);
+    // }
 
-        if(this.state.stars>=5){
-            return ;
-        }
-        // setState() update the value as well as rerender the Component automatically....
+    // handleFavorite =()=>{
+    //     this.setState({
+    //         fav : !this.state.fav
+    //     })
+    // }
 
-        // setState() can be used in two forms-->>
-
-        // FORM 1-->>
-        // this.setState({
-        //     stars : this.state.stars+=0.5
-        // });
-
-        // FORM 2-->>
-        this.setState((prevState)=>{
-            return {
-                stars : prevState.stars+=0.5
-            }
-        })
-
-        // this.state.stars+=0.5; // Can't simply do like this, we also have to rerender the Component
-        console.log(this.state);
-    }
-
-    minusStars = ()=>{
-        if(this.state.stars<=0){
-            return ;
-        }
-        this.setState((prevState)=>{
-            return {
-                stars : prevState.stars-=0.5
-            }
-        })
-        console.log(this.state);
-    }
+    // handleCart =()=>{
+    //     this.setState({
+    //         cart : !this.state.cart
+    //     })
+    // }
 
     render(){
-        const {title,plot,price,rating,stars} = this.state;
+        // const {title,plot,price,rating,stars,fav,cart} = this.props;
+        const {movies} = this.props;
+        const {title,plot,poster,price,rating,stars,fav,cart} = movies;
+        const {addStars , minusStars, handleFavorite, handleCart} = this.props;
 
         return (
             <>
             <div className="main">
                 <div className="movie-card">
                     <div className="left">
-                        <img src="https://media.themoviedb.org/t/p/w500/g3hk2wEeIsTGhh7JvK8yWFVR7ue.jpg" alt="poster"/>
+                        <img src={poster} alt="poster"/>
                     </div>
                     <div className="right">
                         <div className="title">{title}</div>
@@ -73,13 +76,15 @@ export default class MovieCard extends React.Component{
                         <div className="footer">
                             <div className="rating">{rating}</div>
                             <div className="star-dis">
-                                <img src="https://cdn-icons-png.flaticon.com/128/2732/2732689.png" alt="minus" className="str-btn" onClick={this.minusStars}/>
+                                <img src="https://cdn-icons-png.flaticon.com/128/2732/2732689.png" alt="minus" className="str-btn" onClick={()=>minusStars(movies)}/>
                                 <img src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" alt="star" className="stars"/>
-                                <img src="https://cdn-icons-png.flaticon.com/128/748/748113.png" alt="plus" className="str-btn" onClick={this.addStars}/>
+                                <img src="https://cdn-icons-png.flaticon.com/128/748/748113.png" alt="plus" className="str-btn" onClick={()=>addStars(movies)}/>
                                 <span className="starCount">{stars}</span>
                             </div>
-                            <button className="favourite-btn">Add to fav</button>
-                            <button className="cart-btn">Add to Cart</button>
+                           {fav ? <button className="unfavourite-btn" onClick={()=>handleFavorite(movies)}>Unfavorite</button> :
+                                   <button className="favourite-btn" onClick={()=>handleFavorite(movies)}>Favourite</button>}
+                                   
+                            {<button className={cart?"unfavourite-btn":"cart-btn"} onClick={()=>handleCart(movies)}>{cart?"Remove":"Add to Cart"}</button>}
                         </div>
                     </div>
                 </div>
